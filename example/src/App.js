aedc9adcf398d21159891b8aea7042592bc7132f
Ignacy <8687205+lopogo59@users.noreply.github.com>
2019-07-12 13:03:15 +0200

    Almost done

--- example/src/App.js
+++ example/src/App.js
@@ -7,23 +7,55 @@ export default class App extends Component {
     super(props);
     this.state = {
       tempUnits: 'C',
-      appId: 'e1b9c3c4f419e12a1707f7954069e310'
+      appId: 'e1b9c3c4f419e12a1707f7954069e310',
+      city: 'New York'
     };
   }
   componentDidMount() {
+    this.getLocation();
+  }
+  getLocation = (e) => {
     navigator.geolocation.getCurrentPosition(position => {
       this.setState({
         latitude: position.coords.latitude,
         longitude: position.coords.longitude
       })
-    });
+    },
+      error => {
+        alert("There was an error while trying to get the location of the device. Check console for more details.")
+        console.log(error)
+      }, { timeout: 10000 });
+
+  }
+  handleCityInput = (e) => {
+    let cityName = e.target.value;
+    clearTimeout(this.inputTimeout)
+    this.inputTimeout = setTimeout(() => {
+      console.log(cityName)
+      this.setState({
+        city: cityName
+      })
+    }, 800)
+
   }
   render() {
     return (
-      <div>
-        {(this.state.longitude && this.state.latitude) &&
-          <div id="weather"><SimpleReactWeather unit={this.state.tempUnits} lat={this.state.latitude} lon={this.state.longitude} appid={this.state.appId} /></div>
-        }
+      <div className="center">
+        <div className="boxes boxCity">
+          <h2>Enter City Name</h2>
+          <input onInput={this.handleCityInput} id="city" name="city" placeholder="E.g. New York" />
+          {(this.state.city) &&
+            <div id="weatherCity"><SimpleReactWeather unit={this.state.tempUnits} city={this.state.city} appid={this.state.appId} /></div>
+          }
+        </div>
+        <div className="boxes boxLocation">
+          <h2>Based On Your Current Location</h2>
+          <button onClick={this.getLocation} id="getLocation">Request Location</button>
+          {(this.state.longitude && this.state.latitude) &&
+            <div id="weatherLocation"><SimpleReactWeather unit={this.state.tempUnits} lat={this.state.latitude} lon={this.state.longitude} appid={this.state.appId} /></div>
+          }
+        </div>
+
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import './styles.css'
+import style from './styles.css'
 import WeatherIcon from 'react-icons-weather';
 const callApi = async (url) => {
 
@@ -30,52 +30,60 @@ class SimpleReactWeather extends Component {
       };
   }
   componentDidMount() {
-      if (this.props.lat && this.props.lon) {
-          callApi(`http://api.openweathermap.org/data/2.5/weather?lat=${this.props.lat}&lon=${this.props.lon}&appid=${this.props.appid}`,
-              {
-                  headers: {
-                      'Accept': 'application/json',
-                      'Content-Type': 'application/json'
-                  },
-                  method: "GET"
-              })
-              .then(res => {
-                  this.setState({
-                      temp: res.main.temp,
-                      city: res.name,
-                      icon: res.weather[0].id
-                  })
-              })
-              .catch(res => { console.log(res) })
+    this.callOWM();
+  }
+  componentDidUpdate(prevProps) {
+      if ((this.props.city!== prevProps.city) || (this.props.lat!== prevProps.lat) || (this.props.lon !== prevProps.lon)) {
+        this.callOWM();
       }
-      else if (this.props.city) {
-          callApi(`http://api.openweathermap.org/data/2.5/weather?q=${this.props.city}&appid=${this.props.appid}`,
-              {
-                  headers: {
-                      'Accept': 'application/json',
-                      'Content-Type': 'application/json'
-                  },
-                  method: "GET"
-              })
-              .then(res => {
-                  this.setState({                        
-                      temp: res.main.temp,
-                      city: res.name,
-                      icon: res.weather[0].id
-                  })
-              })
-              .catch(res => { console.log(res) })
-      }
-
+    
+  }
+  callOWM = () => {
+    if (this.props.lat && this.props.lon) {
+        callApi(`http://api.openweathermap.org/data/2.5/weather?lat=${this.props.lat}&lon=${this.props.lon}&appid=${this.props.appid}`,
+            {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                method: "GET"
+            })
+            .then(res => {
+                this.setState({
+                    temp: res.main.temp,
+                    city: res.name,
+                    icon: res.weather[0].id
+                })
+            })
+            .catch(res => { console.log(res) })
+    }
+    else if (this.props.city) {
+        callApi(`http://api.openweathermap.org/data/2.5/weather?q=${this.props.city}&appid=${this.props.appid}`,
+            {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                method: "GET"
+            })
+            .then(res => {
+                this.setState({                        
+                    temp: res.main.temp,
+                    city: res.name,
+                    icon: res.weather[0].id
+                })
+            })
+            .catch(res => { console.log(res) })
+    }
   }
   render() {
       return (
-          <div {...this.props} className="simpleReactWeather">
-              <div className="simpleReactWeather-city-icon"> 
-              <WeatherIcon className="simpleReactWeather-icon" name="owm" iconId={String(this.state.icon)} flip="horizontal" rotate="90" />
-              <p className="simpleReactWeather-city">{this.state.city}</p>
+          <div {...this.props} className={style.simpleReactWeather}>
+              <div className={style.simpleReactWeatherCityIcon}> 
+              <WeatherIcon className={style.simpleReactWeatherIcon} name="owm" iconId={String(this.state.icon)} flip="horizontal" rotate="90" />
+              <p className={style.simpleReactWeatherCity}>{this.state.city}</p>
               </div>
-              <p className="simpleReactWeather-temp">{this.props.unit == "F" ? Math.round(this.state.temp * (9/5) - 459.67) + "°F": Math.round(this.state.temp - 273.15) + "°C"}</p>
+              <p className={style.simpleReactWeatherTemp}>{this.props.unit == "F" ? Math.round(this.state.temp * (9/5) - 459.67) + "°F": Math.round(this.state.temp - 273.15) + "°C"}</p>
               
           </div>
       );
